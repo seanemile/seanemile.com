c4e3514ce035aac5e7e688c1d949bf2807fad90b
Sean Emile <seanamani.sa@gmail.com>
2022-09-20 16:11:03 +0300

    Finalized Projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,33 +1,69 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Component, OnDestroy} from '@angular/core';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnDestroy {
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+  destroyed = new Subject<void>();
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
+  profileColumSize = 4;
+  profileRowSize = 1;
+
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+  this.breakpointObserver.observe(
+    [
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]
+    )
+    .pipe(takeUntil(this.destroyed))
+    .subscribe(results => {
+      console.log(results);
+    for (const query of Object.keys(results.breakpoints)) {
+        if (results.breakpoints[query]){
+          if(query === "(max-width: 599.98px)"){
+             this.profileColumSize = 4;
+             this.profileRowSize = 1;
+
+          }else if (query  === "(min-width: 600px) and (max-width: 959.98px)"){
+
+             this.profileColumSize = 2;
+             this.profileRowSize = 1;
+
+          } else if(query  === "(min-width: 960px) and (max-width: 1279.98px)"){
+
+             this.profileColumSize = 1;
+             this.profileRowSize = 1;
+
+          } else if(query  === "(min-width: 1280px) and (max-width: 1919.98px)"){
+
+             this.profileColumSize = 1;
+             this.profileRowSize = 1;
+
+          } else if(query  === "(min-width: 1920px)"){
+
+             this.profileColumSize = 1;
+             this.profileRowSize = 1;
+
+          }
+        }
+        }
     })
-  );
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
