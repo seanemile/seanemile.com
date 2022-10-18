e83be6ef3ffc5bfeaf42db9c40195e366f8200fe
Sean Emile <seanamani.sa@gmail.com>
2022-10-18 21:39:11 +0300

    Added footbar and toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 
 // Project Components
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './shared/material.module';
+import { FootbarComponent } from './footbar/footbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 // Angular Firebase
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -19,10 +19,14 @@ import {
 } from '@angular/fire/analytics';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ToolbarComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    FootbarComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MaterialModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
