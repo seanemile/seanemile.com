def7535248cb3fd1db9c7d382b57d926a5ec7d19
Sean Emile <seanamani.sa@gmail.com>
2022-10-18 20:49:09 +0300

    -  Major Refactor. Removed Redundancy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { SocialsComponent } from './socials/socials.component';
 
 const routes: Routes = [
-      { path: 'home', component: HomeComponent},
-      { path: 'socials', component: SocialsComponent},
-      { path: 'projects', component: ProjectsComponent},
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: '**', redirectTo: 'home', pathMatch: 'full' }
-
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,36 +7,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationsComponent } from './navigations/navigations.component';
 import { HomeComponent } from './home/home.component';
-import { SocialsComponent } from './socials/socials.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { MaterialModule } from './shared/material.module';
 
 // Angular Firebase
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService} from '@angular/fire/analytics';
-import { MaterialModule } from './shared/material.module';
-import { ArticlesModule } from './articles/articles.module';
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+} from '@angular/fire/analytics';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationsComponent,
-    HomeComponent,
-    SocialsComponent,
-    ProjectsComponent,
-  ],
+  declarations: [AppComponent, NavigationsComponent, HomeComponent],
   imports: [
     BrowserModule,
-    ArticlesModule,
     AppRoutingModule,
     MaterialModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
     BrowserAnimationsModule,
   ],
-  providers: [
-   ScreenTrackingService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ScreenTrackingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
