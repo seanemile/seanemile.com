31f0c729bcca6eabb5372b869f10563ec1009dab
Sean Emile <seanamani.sa@gmail.com>
2022-11-24 19:16:38 +0300

    Refactored navigation

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,27 @@
+import { Component, HostBinding } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+})
+export class NavigationComponent {
+  private isDark = true;
+
+  @HostBinding('class')
+  get themeClass() {
+    return this.isDark ? 'dark-theme' : 'light-theme';
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
