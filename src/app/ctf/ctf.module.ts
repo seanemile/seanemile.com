3d376b8ac2f2cd74c6541afab2700747fd1155ce
Sean Emile <seanamani.sa@gmail.com>
2022-11-28 21:41:38 +0300

    Added ctf module

---
+++ src/app/ctf/ctf.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CtfRoutingModule } from './ctf-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CtfRoutingModule
+  ]
+})
+export class CtfModule { }
