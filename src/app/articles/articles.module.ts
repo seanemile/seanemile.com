bccd3fe0cfa005afe1fa0eaca371583f20dc69cc
Sean Emile <seanamani.sa@gmail.com>
2022-11-28 18:11:42 +0300

    Added articles component and module

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ArticlesRoutingModule } from './articles-routing.module';
+import { Article1Component } from './article1/article1.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Article1Component
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ArticlesRoutingModule
   ]
 })
 export class ArticlesModule { }
