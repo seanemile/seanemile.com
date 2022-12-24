35990909a6fe2250d49a1f641296562ec0b3a2fb
Sean Emile <seanamani.sa@gmail.com>
2022-12-24 14:23:29 +0300

    Added routing to article2

--- src/app/articles/articles-routing.module.ts
+++ src/app/articles/articles-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Article1Component } from './article1/article1.component';
+import { Article2Component } from './article2/article2.component';
 import { ArticlesDashboardComponent } from './articles-dashboard/articles-dashboard.component';
 
 const routes: Routes = [
-  { path: 'articles', component: ArticlesDashboardComponent },
-  { path: 'article1', component: Article1Component },
+  {
+    path: 'articles',
+    title: 'Articles',
+    component: ArticlesDashboardComponent,
+  },
+  {
+    path: 'article1',
+    title: 'Gas Optimization in Solidity',
+    component: Article1Component,
+  },
+  {
+    path: 'article2',
+    title: 'Vulnerabilities in solidity',
+    component: Article2Component,
+  },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
